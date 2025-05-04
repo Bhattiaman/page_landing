c8b65852123da52e4fcefed8f5c13d1823df95af
Bhattiaman <amanbhatti105@gmail.com>
2025-05-05 00:04:59 +0530

    hello

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        'images.unsplash.com', // For your Unsplash images
+        // Add other domains if you use more image sources
+      ],
+      minimumCacheTTL: 60, // (optional) Cache images for 60 seconds
+    },
+  };
+  
+  export default nextConfig;
